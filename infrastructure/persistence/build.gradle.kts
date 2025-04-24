93337939ed3fe54daa9133c01eb084539771c98e
Gianpaolo Caprara <gianpaolo.caprara@globant.com>
2025-04-24 21:08:24 +0200

    Feat - Added build.gradle for persistence layer.

---
+++ infrastructure/persistence/build.gradle.kts
@@ -0,0 +1,17 @@
+tasks.named<Jar>("jar") {
+    archiveBaseName.set("purchase-cart-service-infrastructure-persistence")
+}
+
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("plugin.spring")
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
