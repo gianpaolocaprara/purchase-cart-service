3aadb34e0c5a5ffa99452f30f39378fbad0c7963
Gianpaolo Caprara <gianpaolo.caprara@globant.com>
2025-04-24 11:26:19 +0200

    Fix - Fixed domain tests.

--- domain/src/test/kotlin/com/gianpaolo/caprara/purchase/cart/domain/models/OrderItemTest.kt
+++ domain/src/test/kotlin/com/gianpaolo/caprara/purchase/cart/domain/models/OrderItemTest.kt
@@ -8,11 +8,11 @@ class OrderItemTest {
     @Test
     fun `create product order model expected values`() {
         val orderItem = OrderItem(
-            product = Product(id = "1", name = "Product 1", price = 10.0, vat = 22.0),
+            product = Product(id = 1, name = "Product 1", price = 10.0, vat = 22.0),
             quantity = 10
         )
 
-        assertThat(orderItem.product.id).isEqualTo("1")
+        assertThat(orderItem.product.id).isEqualTo(1)
         assertThat(orderItem.product.name).isEqualTo("Product 1")
         assertThat(orderItem.product.price).isEqualTo(10.0)
         assertThat(orderItem.product.vat).isEqualTo(22.0)
