f000f29649251b99daebe4892585585eb19c25d4
Gianpaolo Caprara <gianpaolo.caprara@globant.com>
2025-04-24 12:10:10 +0200

    Feat - Implemented first version of use case using fixed data.

--- domain/src/main/kotlin/com/gianpaolo/caprara/purchase/cart/domain/usecases/CreateOrderUseCase.kt
+++ domain/src/main/kotlin/com/gianpaolo/caprara/purchase/cart/domain/usecases/CreateOrderUseCase.kt
@@ -1,9 +1,34 @@
 package com.gianpaolo.caprara.purchase.cart.domain.usecases
 
 import com.gianpaolo.caprara.purchase.cart.domain.models.Order
+import com.gianpaolo.caprara.purchase.cart.domain.models.OrderItem
+import com.gianpaolo.caprara.purchase.cart.domain.models.Product
 
 class CreateOrderUseCase {
-    fun apply(order: Order): Order{
-        return Order(id = 111, items = order.items)
+    fun apply(order: Order): Order {
+        val existingProducts: List<Product> = listOf(
+            Product(id = 1, price = 20.0, vat = 0.20),
+            Product(id = 2, price = 3.0, vat = 0.30),
+            Product(id = 3, price = 5.0, vat = 0.50),
+        )
+
+        val newOrderItem: List<OrderItem> =
+            order.items.map { item ->
+                val product: Product = existingProducts.first { product -> item.product.id == product.id }
+                OrderItem(
+                    product = Product(
+                        id = product.id,
+                        price = product.price!! * item.quantity,
+                        vat = product.vat!! * item.quantity
+                    ),
+                    quantity = item.quantity
+                )
+            }
+        return Order(
+            id = 111,
+            items = newOrderItem,
+            price = newOrderItem.sumOf { it.product.price!! },
+            vat = newOrderItem.sumOf { it.product.vat!! }
+        )
     }
 }
---
+++ domain/src/test/kotlin/com/gianpaolo/caprara/purchase/cart/domain/usecases/CreateOrderUseCaseTest.kt
@@ -0,0 +1,47 @@
+package com.gianpaolo.caprara.purchase.cart.domain.usecases
+
+import com.gianpaolo.caprara.purchase.cart.domain.models.Order
+import com.gianpaolo.caprara.purchase.cart.domain.models.OrderItem
+import com.gianpaolo.caprara.purchase.cart.domain.models.Product
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class CreateOrderUseCaseTest {
+
+    private lateinit var createOrderUseCase: CreateOrderUseCase
+
+    @BeforeEach
+    fun `set up`() {
+        createOrderUseCase = CreateOrderUseCase()
+    }
+
+    @Test
+    fun `test apply should return expected order`() {
+        val order = Order(
+            items = listOf(
+                OrderItem(product = Product(id = 1), quantity = 1),
+                OrderItem(product = Product(id = 2), quantity = 2),
+                OrderItem(product = Product(id = 3), quantity = 1),
+            )
+        )
+        val orderCreated: Order = createOrderUseCase.apply(order = order)
+
+        assertThat(orderCreated.id).isEqualTo(111)
+        assertThat(orderCreated.price).isEqualTo(31.00)
+        assertThat(orderCreated.vat).isEqualTo(1.30)
+        assertThat(orderCreated.items.size).isEqualTo(3)
+        assertThat(orderCreated.items[0].product.id).isEqualTo(1)
+        assertThat(orderCreated.items[0].product.price).isEqualTo(20.00)
+        assertThat(orderCreated.items[0].product.vat).isEqualTo(0.20)
+        assertThat(orderCreated.items[0].quantity).isEqualTo(1)
+        assertThat(orderCreated.items[1].product.id).isEqualTo(2)
+        assertThat(orderCreated.items[1].product.price).isEqualTo(6.00)
+        assertThat(orderCreated.items[1].product.vat).isEqualTo(0.60)
+        assertThat(orderCreated.items[1].quantity).isEqualTo(2)
+        assertThat(orderCreated.items[2].product.id).isEqualTo(3)
+        assertThat(orderCreated.items[2].product.price).isEqualTo(5.00)
+        assertThat(orderCreated.items[2].product.vat).isEqualTo(0.5)
+        assertThat(orderCreated.items[2].quantity).isEqualTo(1)
+    }
+}
