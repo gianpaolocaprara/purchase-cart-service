0ea992e32fb7ad714403ecbb792a35bc7255f0ee
Gianpaolo Caprara <gianpaolo.caprara@globant.com>
2025-04-25 22:11:13 +0200

    Refactor - Removed unused src folder.

--- src/test/kotlin/com/gianpaolo/caprara/purchase/cart/TestcontainersConfiguration.kt
+++
@@ -1,6 +0,0 @@
-package com.gianpaolo.caprara.purchase.cart
-
-import org.springframework.boot.test.context.TestConfiguration
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration
