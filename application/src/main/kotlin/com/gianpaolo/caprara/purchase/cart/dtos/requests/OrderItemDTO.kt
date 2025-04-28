e4375174db0d19b8f0abd43721be3adb60dd6a1f
Gianpaolo Caprara <gianpaolo.caprara@globant.com>
2025-04-28 11:58:13 +0200

    Docs - Added some info for swagger in DTOs.

--- application/src/main/kotlin/com/gianpaolo/caprara/purchase/cart/dtos/requests/OrderItemDTO.kt
+++ application/src/main/kotlin/com/gianpaolo/caprara/purchase/cart/dtos/requests/OrderItemDTO.kt
@@ -1,9 +1,12 @@
 package com.gianpaolo.caprara.purchase.cart.dtos.requests
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
 
 class OrderItemDTO(
+    @Schema(description = "The product id", example = "1")
     @field:JsonProperty("product_id")
     val productId: Int,
+    @Schema(description = "The quantity (cannot be negative or zero)", example = "3")
     val quantity: Int
 )
